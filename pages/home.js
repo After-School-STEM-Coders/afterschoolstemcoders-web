27deadbd502dd54f4b590236493c1e430b5eac62
Night Fox <Johnnyh961@gmail.com>
2021-05-26 19:51:42 -0500

    made home page

---
+++ pages/home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <>
+      <div className="h-screen bg-gray-500 flex justify-center items-center">
+        <div className="text-3xl text-center">Coming Soon!</div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
