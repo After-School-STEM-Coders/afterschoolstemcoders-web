a32098161911bbba1b4c6c514fb5db868c66dc62
NightFox001 <johnnyh961@gmail.com>
2021-05-28 16:47:18 -0500

    made basic layout of header component

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -8,8 +8,21 @@ export const Header = () => {
   };
   return (
     <div>
-      <div className="text-center">Example 1</div>
-      <button onClick={handleClick}>click here to see example2</button>
+      <div className="p-4 flex flex-row justify-betweend fixed text-white bg-gray-700 bg-opacity-80 w-full">
+        <div className=" space-x-8 flex flex-row flex-wrap items-center w-full max-w-screen-xl justify-center">
+          <div className="text-7xl">CSC</div>
+
+          <div className="flex flex-1 items-center justify-end">
+            <div className="space-x-16 flex flex-1 mr-8 font-semibold justify-center">
+              <div className="cursor-pointer">Page</div>
+              <div className="cursor-pointer">Page</div>
+              <div className="cursor-pointer">Page</div>
+              <div className="cursor-pointer">Page</div>
+              <div className="cursor-pointer">Page</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/home.tsx
+++ pages/home.tsx
@@ -2,7 +2,7 @@ import { Header } from "../components/organisms/Header";
 
 const Home = () => {
   return (
-    <div className="h-screen bg-gray-700 flex justify-center items-center">
+    <div className="h-screen bg-gray-900 ">
       <Header />
     </div>
   );
