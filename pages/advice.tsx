c6cc39b27fee5aed83c545bd6e747b54ab3d0b74
jr7072 <rosales.juan7072@gmail.com>
2021-05-29 00:38:03 -0500

    created advice page

---
+++ pages/advice.tsx
@@ -0,0 +1,14 @@
+import { Header } from "../components/organisms/Header";
+
+const Advice = () => {
+
+    return (
+        <div className="h-screen bg-blue-900" >
+            <Header />
+            
+        </div>
+    );
+    
+}
+
+export default Advice;
